3e784c9d5db924a943d181dbc14626234bd1ccc5
Tao Zeng <TaoZeng@my.uopeople.edu>
2024-09-15 18:42:03 -0700

    Add new methods for shopping cart-related operations

--- Unit 2/ecommerce-system/src/com/ecommerce/cart/ShoppingCart.java
+++ Unit 2/ecommerce-system/src/com/ecommerce/cart/ShoppingCart.java
@@ -18,14 +18,24 @@ public class ShoppingCart {
         return this.items;
     }
 
-    // Other methods
+    // Add products to the shopping cart
     public void addProduct(Product product, int quantity) {
-        this.items.put(product, quantity);
+        // put() method would simply overwrite the existing quantity if the product is already in the cart.
+        // merge() method adds the new quantity to the existing quantity if the product is already in the cart.
+        // The first argument product is the key.
+        // The second argument quantity is the value to be merged.
+        // The third argument Integer::sum is a function that defines how to merge the values.
+        this.items.merge(product, quantity, Integer::sum);
     }
 
+    // Remove products from the shopping cart
     public void removeProduct(Product product) {
-        // Remove an element from the HashMap
         this.items.remove(product);
+    }
+
+    // Clear and remove all the products
+    public void clearProducts() {
+        this.items.clear();
     }
 
     public void updateQuantity(Product product, int newQuantity) {

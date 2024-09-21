02623bafb138e0f39734ac3a89ac8eee062a9ca5
Tao Zeng <TaoZeng@my.uopeople.edu>
2024-09-20 17:04:38 -0700

    Update the Clock class

--- Unit 3/clock/src/Clock.java
+++ Unit 3/clock/src/Clock.java
@@ -1,35 +1,25 @@
+// https://www.w3schools.com/java/java_date.asp
+// java.time classes are the modern replacement for the troublesome old legacy date-time classes bundled with the earliest versions of Java
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 
+// Clock class responsible for managing and displaying current time and date
 public class Clock {
-    private LocalDateTime currentTime;
-    private DateTimeFormatter timeFormatter;
-    private DateTimeFormatter dateFormatter;
+    private String currentTime;
+    private final DateTimeFormatter formatter;
 
     // Constructor Declaration of Class
-    public Clock() {
-        this.currentTime = LocalDateTime.now();
-        this.timeFormatter = DateTimeFormatter.ofPattern("KK:mma");
-        this.dateFormatter = DateTimeFormatter.ofPattern("cccc, MMMM dd, yyyy");
+    public Clock(String timeFormat) {
+        // The formatter is initialized based on the input parameter passed to the constructor.
+        this.formatter = DateTimeFormatter.ofPattern(timeFormat);
+        this.currentTime = LocalDateTime.now().format(this.formatter);
     }
 
-    // Getters
-    public String getTime() {
-        return this.currentTime.format(this.timeFormatter);
+    public String getCurrentTime() {
+        return this.currentTime;
     }
 
-    public String getDate() {
-        return this.currentTime.format(this.dateFormatter);
-    }
-
-    // Setters
-    public void updateTime(String time) {
-        this.currentTime = LocalDateTime.now();
-    }
-
-    // Other methods
-    public void displayTimeAndDate() {
-        System.out.println("Current time: " + this.getTime());
-        System.out.println("Current date: " + this.getDate());
+    private void updateTime() {
+        this.currentTime = LocalDateTime.now().format(this.formatter);
     }
 }

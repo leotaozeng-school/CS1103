f0455b014b3d8f0354e096287c00bc021ab6c165
Tao Zeng <TaoZeng@my.uopeople.edu>
2024-10-20 16:56:24 +0800

    feat: create Java Client Socket

--- Unit 7/online-chat-application/src/ChatClient.java
+++ Unit 7/online-chat-application/src/ChatClient.java
@@ -1,4 +1,52 @@
-package client;
+import java.io.*;
+import java.net.*;
 
+/**
+ * This class implements java socket client
+ * @author Tao Zeng
+ *
+ */
 public class ChatClient {
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 5000;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public void start() throws IOException {
+        socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+
+        // Setting up input and output streams
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Start a thread to handle incoming messages from the server
+        new Thread(new MessageReader()).start();
+
+        System.out.println("Connected to the chat server.");
+        System.out.println("Type your messages. Type 'exit' to quit.");
+    }
+
+    public void sendMessage(String message) {
+        out.println(message);
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
+    private class MessageReader implements Runnable {
+        public void run() {
+            String serverResponse;
+            try {
+                while ((serverResponse = in.readLine()) != null) {
+                    System.out.println(serverResponse);
+                }
+            } catch (IOException e) {
+                System.out.println("Error reading from server: " + e.getMessage());
+            }
+        }
+    }
 }
--- Unit 7/online-chat-application/src/ChatServer.java
+++ Unit 7/online-chat-application/src/ChatServer.java
@@ -1,13 +1,46 @@
-package server;
+import java.io.*;
+import java.net.*;
 
 /**
- * This class implements java Socket server
- * @author Tao Zeng
+ * This class implements Java Socket server to handle multiple Clients with socket connections
  *
+ * @author Tao Zeng
  */
 public class ChatServer {
-    //static ServerSocket variable
-    private static ServerSocket server;
-    //socket server port on which it will listen
-    private static int port = 9876;
+    // socket server port on which it will listen
+    private static final int PORT = 5000;
+
+    public void start() throws Exception {
+        System.out.println("Chat Server is running on port " + PORT);
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        Socket clientSocket = serverSocket.accept();
+        ClientHandler clientHandler = new ClientHandler(clientSocket);
+    }
+
+    // Run method to handle client communication
+    private class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private BufferedReader in;
+        private PrintWriter out;
+        private int userId;
+
+        // Constructor
+        public ClientHandler(Socket socket, int userId) throws IOException {
+            // Create input and output streams for communication
+            this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            this.out = new PrintWriter(clientSocket.getOutputStream(), true);
+            this.clientSocket = socket;
+            this.userId = userId;
+        }
+
+        // Run method to handle client communication
+        @Override
+        public void run() {
+            try {
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }

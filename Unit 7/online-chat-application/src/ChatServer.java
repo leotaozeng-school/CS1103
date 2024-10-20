a10412781af4c633666704f6de9b05914eb8fb4b
Tao Zeng <TaoZeng@my.uopeople.edu>
2024-10-20 16:55:10 +0800

    feat: create Java Client Socket

---
+++ Unit 7/online-chat-application/src/ChatServer.java
@@ -0,0 +1,13 @@
+package server;
+
+/**
+ * This class implements java Socket server
+ * @author Tao Zeng
+ *
+ */
+public class ChatServer {
+    //static ServerSocket variable
+    private static ServerSocket server;
+    //socket server port on which it will listen
+    private static int port = 9876;
+}
